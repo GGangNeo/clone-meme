0b5f811911b8533ff14709d90a79696689de70ff
GGangNeo <107779482+GGangNeo@users.noreply.github.com>
2022-07-19 17:17:08 +0900

    drawing project one, two
    home, doll

--- src/app.js
+++ src/app.js
@@ -3,35 +3,34 @@ const ctx = canvas.getContext("2d");
 
 console.dir(ctx);
 
+const XOFFSET_HOME = -50;
+const YOFFSET_HOME = -25;
+
 canvas.width = 800;
 canvas.height = 800;
+ctx.fillRect(200 + XOFFSET_HOME, 200 + YOFFSET_HOME, 50, 200);
+ctx.fillRect(400 + XOFFSET_HOME, 200 + YOFFSET_HOME, 50, 200);
+ctx.fillRect(300 + XOFFSET_HOME, 300 + YOFFSET_HOME, 50, 100);
+ctx.fillRect(200 + XOFFSET_HOME, 200 + YOFFSET_HOME, 200, 20);
+ctx.moveTo(200 + XOFFSET_HOME, 200 + YOFFSET_HOME);
+ctx.lineTo(325 + XOFFSET_HOME, 100 + YOFFSET_HOME);
+ctx.lineTo(450 + XOFFSET_HOME, 200 + YOFFSET_HOME);
+ctx.fill();
 
-// ctx.fillRect(100, 100, 100, 100);
 
-//Use rect + fill => fillRect
-// ctx.rect(300, 300, 100, 100);
-// ctx.rect(50, 50, 100, 100);
-// ctx.rect(150, 150, 100, 100);
-// ctx.rect(250, 250, 100, 100);
-// ctx.fill();
+const xOffset = 320;
+const yOffset = 320;
 
-// ctx.rect(350, 350, 100, 100);
-// ctx.rect(450, 450, 100, 100);
-// ctx.fillStyle = "red";
-// ctx.fill();
-
-// Use lineTo + moveTo => rect t
-
-// set start postiion
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.stroke();
-
-// Start new line
 ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.lineTo(200, 200);
-ctx.lineTo(300, 200);
-ctx.lineTo(300, 100);
-ctx.stroke();
+ctx.fillRect(170 + xOffset, 170 + yOffset, 15, 100);
+ctx.fillRect(310 + xOffset, 170 + yOffset, 15, 100);
+ctx.fillRect(220 + xOffset, 170 + yOffset, 60, 200);
+
+ctx.arc(250 + xOffset, 100 + yOffset, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270 + xOffset, 80 + yOffset, 8, Math.PI, 2 * Math.PI);
+ctx.arc(230 + xOffset, 80 + yOffset, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
